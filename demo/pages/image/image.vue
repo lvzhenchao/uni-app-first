d01ca0904f83a2bb7ccc258fe69b6a3a583e3ecc
锐源 <1370164516@qq.com>
2023-09-19 00:30:19 +0800

    媒体组件

---
+++ demo/pages/image/image.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<image src="../../static/logo.png" mode=""></image>
+		<audio style="text-align: left" :src="current.src" :poster="current.poster" :name="current.name" :author="current.author" :action="audioAction" controls></audio>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				current: {
+					poster: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/music-a.png',
+					name: '致爱丽丝',
+					author: '暂无',
+					src: 'https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-hello-uniapp/2cc220e0-c27a-11ea-9dfb-6da8e309e0d8.mp3',
+				},
+				audioAction: {
+					method: 'pause'
+				}
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
