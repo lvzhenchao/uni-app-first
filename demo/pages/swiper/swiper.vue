99e1457e2a7618be84aa37531d931ed82feeed7a
锐源 <1370164516@qq.com>
2023-09-18 23:13:58 +0800

    swiper

---
+++ demo/pages/swiper/swiper.vue
@@ -0,0 +1,50 @@
+<!-- 
+轮播图
+快捷键：usw
+ -->
+<template>
+	<view>
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item>
+				<view class="v1"></view>
+			</swiper-item>
+			<swiper-item>
+				<view class="v2"></view>
+			</swiper-item>
+			<swiper-item>
+				<view class="v3"></view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.v1 {
+			width: 100%;
+			height: 300rpx;
+			background-color: red;
+		}
+	.v2 {
+		width: 100%;
+		height: 300rpx;
+		background-color: blue;
+	}
+	.v3 {
+		width: 100%;
+		height: 300rpx;
+		background-color: gold;
+	}
+</style>
