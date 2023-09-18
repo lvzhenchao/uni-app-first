02824995bc61692661d5b8a927b9eb2bcd897249
锐源 <1370164516@qq.com>
2023-09-19 00:49:28 +0800

    form表单

---
+++ demo/pages/form/form.vue
@@ -0,0 +1,39 @@
+<!-- 
+@ 代表事件，vue的语法 
+ -->
+<template>
+	<view>
+		<button type="default">按钮</button>
+		<label>
+			<checkbox value="cb"  checked="true"/><text>选中</text>
+		</label>
+		<editor placeholder="富文本编辑器"></editor>
+		<form @submit="" @reset="">
+			<text>用户名:{{username}}</text>
+			<input type="text" @input="input1" value="" />
+			<button form-type="submit">Submit</button>
+			<button type="default" form-type="reset">Reset</button>
+		</form>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				username: ''
+			}
+		},
+		methods: {
+			input1:function(e){
+				this.username = e.detail.value
+			}
+		}
+	}
+</script>
+
+<style>
+	input {
+		background-color: gray;
+	}
+</style>
