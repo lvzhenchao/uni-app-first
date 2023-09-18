e41d888e263f7a292ff2749e07387d946335323f
锐源 <1370164516@qq.com>
2023-09-18 23:05:22 +0800

    view & scroll-x-y

---
+++ demo/pages/scrool/scrool.vue
@@ -0,0 +1,68 @@
+<template>
+	<!-- <scroll-view class="scroll-y" scroll-y="true" >
+		<view class="v1">v1</view>
+		<view class="v2">v2</view>
+		<view class="v3">v3</view>
+	</scroll-view> -->
+	<scroll-view class="scroll-x" scroll-x="true" >
+		<view class="vv1">vv1</view>
+		<view class="vv2">vv2</view>
+		<view class="vv3">vv3</view>
+	</scroll-view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.scroll-x {
+		white-space: nowrap;
+	}
+	.vv1 {
+		display: inline-block;
+		width: 100%;
+		height: 300rpx;
+		background-color: red;
+	}
+	.vv2 {
+		display: inline-block;
+		width: 100%;
+		height: 300rpx;
+		background-color: blue;
+	}
+	.vv3 {
+		display: inline-block;
+		width: 100%;
+		height: 300rpx;
+		background-color: gold;
+	}
+	
+	.scroll-y {
+		height: 300rpx;
+	}
+	.v1 {
+		width: 100%;
+		height: 300rpx;
+		background-color: red;
+	}
+	.v2 {
+		width: 100%;
+		height: 300rpx;
+		background-color: blue;
+	}
+	.v3 {
+		width: 100%;
+		height: 300rpx;
+		background-color: gold;
+	}
+</style>
---
+++ demo/pages/view/view.vue
@@ -0,0 +1,55 @@
+<!-- 
+类似div,用于包裹各种元素内容
+属性：
+hover-class 指定按下去的样式类
+hover-stop-propagation 阻止本节点的祖先节点出现点击态，阻止冒泡
+ -->
+<template>
+	<!-- <view class="" hover-class="v1">hello lampol</view> -->
+	<view class="v2">
+		<view hover-class="v1">
+			hello lzc
+		</view>
+	</view>
+	
+	<view hover-class="v4" class="v2">
+		<view hover-class="v3" class="v1" hover-stop-propagation>
+			hello lampol
+		</view>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.v1 {
+		background-color: blue;
+	}
+	.v2 {
+		width: 200rpx;
+		height: 200rpx;
+		background-color: aqua;
+	}
+	.v3 {
+		width: 100rpx;
+		height: 100rpx;
+		background-color: palegreen;
+	}
+	.v4 {
+		width: 100rpx;
+		height: 100rpx;
+		background-color: royalblue;
+	}
+</style>
