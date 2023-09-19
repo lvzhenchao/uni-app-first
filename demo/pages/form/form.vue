a55071262a76c1629d9b152c9e55fa581ac18f21
锐源 <1370164516@qq.com>
2023-09-19 12:29:16 +0800

    单选和复选框

--- demo/pages/form/form.vue
+++ demo/pages/form/form.vue
@@ -8,6 +8,20 @@
 			<checkbox value="cb"  checked="true"/><text>选中</text>
 		</label>
 		<editor placeholder="富文本编辑器"></editor>
+		
+		<text>复选框</text>
+		<checkbox-group name="" @change="chChange">
+			<label>
+				<checkbox value="php" /><text>php</text>
+			</label>
+			<label>
+				<checkbox value="java" /><text>java</text>
+			</label>
+			<label>
+				<checkbox value="golang" /><text>golang</text>
+			</label>
+		</checkbox-group>
+		
 		<form @submit="" @reset="">
 			<text>用户名:{{username}}</text>
 			<input type="text" @input="input1" value="" />
@@ -27,6 +41,9 @@
 		methods: {
 			input1:function(e){
 				this.username = e.detail.value
+			},
+			chChange:function(e){
+				console.log(e.detail.value)
 			}
 		}
 	}
