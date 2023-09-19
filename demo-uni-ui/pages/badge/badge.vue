c29e98d94f246cf7e3cbbc9b0859756b6a18be46
锐源 <1370164516@qq.com>
2023-09-19 22:44:13 +0800

    uni-ui

---
+++ demo-uni-ui/pages/badge/badge.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<uni-badge text="1"></uni-badge>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
