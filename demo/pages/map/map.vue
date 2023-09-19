97f9f236e766ab7bab88fe2392378af9474267a3
锐源 <1370164516@qq.com>
2023-09-19 14:13:06 +0800

    webview&&map组件

---
+++ demo/pages/map/map.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<map class="map" ></map>
+		<web-view src="https://www.baidu.com"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.map{
+		width: 100%;
+		height: 300rpx;
+	}
+</style>
